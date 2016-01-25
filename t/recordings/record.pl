8942d4047b786f8f260b844b3408e6610b9eb2cb
Erik Tank <erik.tank@ticketmaster.com>
2016-01-25 14:50:48 -0700

    Update record.pl to make recording to a test API key easier.

--- t/recordings/record.pl
+++ t/recordings/record.pl
@@ -10,14 +10,14 @@ use Storable;
 
 use Ticketmaster::API;
 
-my $api_key  = 'testAPIkey';
+my $real_api_key  = 'realAPIkey';
+my $save_api_key  = 'testAPIkey';
 my $mode = 'GET';
-my $path_template = 'discovery/%s/events.json';
+my $path_template = 'discovery/%s/venues/341396.json';
 my %parameters = (
-    marketId => 101
 );
 
-my $recording_name = 'GET_' . sprintf($path_template, 'v1') . "?apikey=$api_key";
+my $recording_name = 'GET_' . sprintf($path_template, 'v1') . "?apikey=$save_api_key";
 foreach my $key (sort {$a cmp $b} keys %parameters) {
     $recording_name .= '_' . $key . '_' . $parameters{$key};
 }
@@ -26,7 +26,7 @@ $recording_name =~ s/[=?&\/]/_/g;
 die("record.pl needs to be run within the recordings directory") unless $0 eq 'record.pl';
 die("recording already exists: $recording_name\n") if -e $recording_name;
 
-my $obj = Ticketmaster::API->new(api_key => $api_key);
+my $obj = Ticketmaster::API->new(api_key => $real_api_key);
 my $res = $obj->get_data(mode => $mode, path_template => $path_template);
 
 store $res, $recording_name;
